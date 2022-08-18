5f8317e199de8943e8d97bd19a420769bc619fc0
jyoati <jyoatimagade@gmail.com>
2022-08-18 15:05:45 +0530

    5. added global styled

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,61 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span>
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>come true</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact Us for any Photography or videography ideas that you have. We
           have professionals with amazing skills
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="guy with a camerea" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+//style components
 
+// const About = styled.div`
+//   min-height: 90vh;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding: 5rem 10rem;
+//   color: #fff;
+// `;
+
+// const Description = styled.div`
+//   flex: 1;
+//   padding-right: 5rem;
+//   h2 {
+//     font-weight: lighter;
+//   }
+// `;
+// const Image = styled.div`
+//   flex: 1;
+//   overflow: hidden;
+//   IMG {
+//     width: 100%;
+//     height: 80vh;
+//     object-fit: cover;
+//   }
+// `;
 export default AboutSection;

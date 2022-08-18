f4e77ea153222ca48fc5f662fa06065d6bb53a66
jyoati <jyoatimagade@gmail.com>
2022-08-18 13:22:48 +0530

    3. service section 4. faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <>
+      <div className="faq">
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <div className="question">
+          <h4>how Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              ipsum, nam molestias dolorem cum, sunt assumenda tempora
+              necessitatibus quasi maxime corporis rem atque incidunt fugit
+              minus repudiandae ea possimus nisi?
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              ipsum, nam molestias dolorem cum, sunt assumenda tempora
+              necessitatibus quasi maxime corporis rem atque incidunt fugit
+              minus repudiandae ea possimus nisi?
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Differnet Payment Method</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              ipsum, nam molestias dolorem cum, sunt assumenda tempora
+              necessitatibus quasi maxime corporis rem atque incidunt fugit
+              minus repudiandae ea possimus nisi?
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What Product Do you Offer</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              ipsum, nam molestias dolorem cum, sunt assumenda tempora
+              necessitatibus quasi maxime corporis rem atque incidunt fugit
+              minus repudiandae ea possimus nisi?
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </div>
+  );
 };
 
 export default AboutUs;
